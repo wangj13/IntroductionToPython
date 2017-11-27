2f160211a72ba631c3d0f680d24aa2cab946a2f6
wangj13 <wangj13@rose-hulman.edu>
2017-11-27 17:15:09 -0500

    I added my name.

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  JIXI WANG  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -73,11 +73,13 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
+matt.right(45)
+matt.forward(30)
 
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +93,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +111,19 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+sss = rg.SimpleTurtle()
+sss.pen = rg.Pen('yellow', 20)
+sss.speed = 15
+sss.backward(30)
+sss.left(45)
+sss.forward(40)
+sss.right(45)
+sss.forward(60)
+
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -5,12 +5,12 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
 """
 ########################################################################
-# TODO: 1.
-# On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
+# DONE: 1.
+# On Line 5 above, replace  JIXI WANG with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,24 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+green_turtle = rg.SimpleTurtle('turtle')
+green_turtle.speed = 10
+green_turtle.pen = rg.Pen('purple', 3)
+
+size = 100
+
+for k in range(10):
+    green_turtle.draw_circle(size)
+    green_turtle.pen_up()
+    green_turtle.right(45)
+    green_turtle.forward(10)
+    green_turtle.left(45)
+    green_turtle.pen_down()
+    size=size-10
+
+window.close_on_mouse_click()
+
